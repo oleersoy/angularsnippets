6c358b8c8cbd0827fe2fc250c24c5aa061f120d2
Ole <ole.ersoy@gmail.com>
2018-05-29 10:20:28 -0500

    Update snippets

--- SNIPPETS.js
+++ SNIPPETS.js
@@ -10,8 +10,11 @@ goOrders() {
 /**
  * MULTI QUERY PARAMETER NAVIGATION EXAMPLE 
  * http://localhost:4200/orders?type=priority&shipping=1day
+ * 
+ * Note the quotes around the 'size-range' parameter.  If they
+ * were not there the - would be interpreted as a minus sign operator.
  */
 
 goOrders() {
-    this.router.navigate(['/orders'], { queryParams: { type: 'priority', shipping: '1day' } });
+    this.router.navigate(['/orders'], { queryParams: { type: 'priority', shipping: '1day' }, 'size-range': 'small-large' });
 }
