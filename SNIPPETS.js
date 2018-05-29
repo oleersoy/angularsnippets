69179ae87454554bfb5e53f6fd910e2ce56b9593
Ole <ole.ersoy@gmail.com>
2018-05-29 10:05:49 -0500

    Some snippets

--- SNIPPETS.js
+++ SNIPPETS.js
@@ -1,5 +1,17 @@
-/** http://localhost:4200/orders?order=priority */
+/**
+ * SINGLE QUERY PARAMETERS NAVIGATION EXAMPLE 
+ * http://localhost:4200/orders?type=priority 
+ */
 
 goOrders() {
-    this.router.navigate(['/orders'], { queryParams: { order: 'priority' } });
+    this.router.navigate(['/orders'], { queryParams: { type: 'priority' } });
+}
+
+/**
+ * MULTI QUERY PARAMETER NAVIGATION EXAMPLE 
+ * http://localhost:4200/orders?type=priority&shipping=1day
+ */
+
+goOrders() {
+    this.router.navigate(['/orders'], { queryParams: { type: 'priority', shipping: '1day' } });
 }
