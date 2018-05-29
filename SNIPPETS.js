398525530f4d0b355202b7dd00ad13e325475bef
Ole <ole.ersoy@gmail.com>
2018-05-29 11:12:48 -0500

    Update snippets

--- SNIPPETS.js
+++ SNIPPETS.js
@@ -18,3 +18,33 @@ goOrders() {
 goOrders() {
     this.router.navigate(['/orders'], { queryParams: { type: 'priority', shipping: '1day' }, 'size-range': 'small-large' });
 }
+
+/**
+ * PRESERVING QUERY PARAMETERS NAVIGATION EXAMPLE 
+ * http://localhost:4200/orders?type=priority&shipping=1day
+ * 
+ * Suppose we are at the above URL and we wish to preserve these
+ * query parameters when navigating to products.
+ * 
+ * http://localhost:4200/products?type=priority&shipping=1day
+ * 
+ */
+
+goProducts() {
+    this.router.navigate(['/products'], { queryParamsHandling: 'preserve' });
+}
+
+  /**
+ * MERGING QUERY PARAMETERS NAVIGATION EXAMPLE 
+ * http://localhost:4200/orders?type=priority&shipping=1day
+ * 
+ * Suppose we are at the above URL and we wish to merge these
+ * query parameters with an elecronics filter parameter navigating to products.
+ * 
+ * http://localhost:4200/products?type=priority&shipping=1day&filter=electronics
+ * 
+ */
+
+goProducts() {
+    this.router.navigate(['/products'], { queryParams: { filter: 'electronics'}, queryParamsHandling: 'merge' });    
+}
